ffbef32dab041675da24dc2581b688dc34ef669b
cvetkovska-a <angela.cvetkovska@htecgroup.com>
2023-04-25 15:48:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from constructs import Construct
+from cdktf import App, TerraformStack, TerraformOutput
+
+
+class MyStack(TerraformStack):
+    def __init__(self, scope: Construct, id: str):
+        super().__init__(scope, id)
+
+        # define resources here
+
+        TerraformOutput(self, "TestStack",value="12")
+
+
+app = App()
+MyStack(app, "cdktf-python")
+
+app.synth()
---
+++ output.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+from constructs import Construct
+from cdktf import App, TerraformStack, TerraformOutput
+
+
+class OutputStack(TerraformStack):
+    def __init__(self, scope: Construct, id: str):
+        super().__init__(scope, id)
+
+        # define resources here
+
+        TerraformOutput(self, "OutputStack",value="12")
+
+
+app = App()
+OutputStack(app, "cdktf-python")
+print("output.py got executed")
+print(dict(os.environ))
+app.synth()
